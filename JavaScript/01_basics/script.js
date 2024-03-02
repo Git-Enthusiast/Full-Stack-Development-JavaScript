473098c7f5d06fffbf883503dd29a9a16c7f225a
Git-Enthusiast <messachuate@gmail.com>
2024-03-03 00:28:47 +0530

    JavaScript Basics Lec 1

--- JavaScript/01_basics/script.js
+++ JavaScript/01_basics/script.js
@@ -1,1 +1,14 @@
-console.log("Hi, This is used to check the working of the git.")
+const accountId = 144533
+let accountEmail = "rajan.cse@outlook.com"
+var accountPassword = "12345"
+// accountId = 2    not Allowed you can not reassign a value in the constant.
+console.log(accountId);
+accountEmail = "messachuate@gmail.com"
+accountPassword = "54321"
+accountCity = "Bengaluru"
+let accountState;
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope.
+*/
