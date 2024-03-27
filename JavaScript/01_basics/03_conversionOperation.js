210726614e9b5cea12cef7952bd82019ee578ec3
Git-Enthusiast <messachuate@gmail.com>
2024-03-28 00:16:52 +0530

    Operations Updated

--- JavaScript/01_basics/03_conversionOperation.js
+++ JavaScript/01_basics/03_conversionOperation.js
@@ -24,6 +24,36 @@ console.log(typeof stringNumber);
 
 // ************** Operations ***************
 let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);
+
+let str1 = "hello"
+let str2 = " Rajan"
+let str3 = str1+str2
+console.log(str3)
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+3+"2");
+console.log(1+3+"2");
+console.log(true); //true
+console.log(+true); //1
+console.log(+""); //0
+
+let num1,num2,num3
+num1 = num2 = num3 = 2+2
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+++gameCounter
+console.log(gameCounter);
+
 
 
 
