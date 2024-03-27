e05ece2d4eec7dc7611878067060db651b11c296
Git-Enthusiast <messachuate@gmail.com>
2024-03-27 23:57:17 +0530

    Type 03_conversionOperation Updated

--- JavaScript/01_basics/03_conversionOperation.js
+++ JavaScript/01_basics/03_conversionOperation.js
@@ -3,4 +3,28 @@ let score = "33"
 
 console.log(typeof(score));
 console.log(typeof score);
+score = "Rajan"
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+let num = "71"
+console.log(num);
+let bool = "true"
+console.log(bool);
+let isLoggedin = "Rajan Raj"
+// if 1 => true
+// if 0 => false
+// if "" => false
+// "Rajan"=> true
+let booleanisLoggedin = Boolean(isLoggedin)
+console.log(booleanisLoggedin);
+let someNumber = 33
+let stringNumber =  String(someNumber)
+console.log(typeof stringNumber);
+
+
+
+
+
+
 
