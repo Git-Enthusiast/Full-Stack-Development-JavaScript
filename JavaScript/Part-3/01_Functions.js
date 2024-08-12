356f41709969b945b42674b6676a0b1198b9fde8
Git-Enthusiast <messachuate@gmail.com>
2024-08-12 21:11:14 +0530

    01_Functions Added

---
+++ JavaScript/Part-3/01_Functions.js
@@ -0,0 +1,44 @@
+// console.log("H");
+// console.log("I");
+// console.log("T");
+// console.log("E");
+// console.log("S");
+// console.log("H");
+// if we have to print it 100 times it will be an overhead 
+// so we use functions in js
+
+function sayMyName(){
+    
+console.log("H");
+console.log("I");
+console.log("T");
+console.log("E");
+console.log("S");
+console.log("H");
+
+}
+//sayMyName()
+
+
+function addTwoNumbers(number1,number2){ // in line 23 number1
+    // and number2 is a parameter 
+    console.log(number1+number2);
+    
+}
+addTwoNumbers(3,5) // here when a value is passed it is called as an arguments.
+
+addTwoNumbers(3,undefined) // NaN
+addTwoNumbers(3,null) // 3
+addTwoNumbers(3,"Rajan")  // Concatination: 3Rajan
+
+const result = addTwoNumbers(5,6)
+l
+
+ 
+
+
+
+
+
+
+
--- JavaScript/Part-3/script.js
+++
@@ -1,1 +0,0 @@
-console.log("His , This is Rajan Raj")
